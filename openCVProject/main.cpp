c20217cba3146c4756d86db0817f5512e053213c
JBladejs <JBladejs@gmail.com>
2019-06-18 17:08:23 +0200

    Added exiting on escape key press

--- openCVProject/main.cpp
+++ openCVProject/main.cpp
@@ -12,10 +12,25 @@
 using namespace cv;
 using namespace std;
 
+bool turned_on;
+
 double getFrameDelay(VideoCapture capture) {
 	double frame_delay = 1000 / capture.get(CV_CAP_PROP_FPS);
 	if (frame_delay == 0) frame_delay = 33.3;
 	return frame_delay;
+}
+
+void interpretKey(int key) {
+	switch(key) {
+		case 27:
+			turned_on = false;
+			break;
+		case -1:
+			break;
+		default:
+			cout << key << endl;
+			break;
+	}
 }
 
 void execute(Mat frame, string window_name) {
@@ -23,12 +38,13 @@ void execute(Mat frame, string window_name) {
 }
 
 void play(string video_name) {
+	turned_on = true;
 	VideoCapture cap(video_name);
 	double frame_delay = getFrameDelay(cap);
 	Mat frame;
 	String window_name;
 	namedWindow(window_name, CV_WINDOW_AUTOSIZE);
-	while (cap.read(frame)) {
+	while (cap.read(frame) && turned_on) {
 		double time = clock();
 		execute(frame, window_name);
 		time -= clock();
@@ -36,6 +52,7 @@ void play(string video_name) {
 		int key;
 		if(delay > 0) key = waitKey(delay);
 		else key = waitKey(1);
+		interpretKey(key);
 	}
 	cap.release();
 	cvDestroyAllWindows();
