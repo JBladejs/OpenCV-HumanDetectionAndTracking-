f032bbee26868d60896beace3995b99ca7b2eede
JBladejs <JBladejs@gmail.com>
2019-06-24 03:25:13 +0200

    additional changes

--- openCVProject/main.cpp
+++ openCVProject/main.cpp
@@ -1,10 +1,10 @@
-﻿#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/objdetect/objdetect.hpp"
-#include "opencv2/core.hpp"
-#include "opencv2/video.hpp"
-#include "opencv2/videoio.hpp"
-#include "opencv2/imgcodecs.hpp"
+﻿#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/video.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/imgcodecs.hpp>
 
 #include <windows.h>
 #include <iomanip>  
